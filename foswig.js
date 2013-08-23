6cf66039288ba7486c78a1fc9c7d5af8bf90dd2f
Glenn <mr.sharpoblunto@gmail.com>
2013-08-23 08:53:20 -0700

    Added ability to specify both min and max lengths for generated words. Added ability to specify whether generated words are allowed to match words in the input dictionary. Added minified version of lib.

--- foswig.js
+++ foswig.js
@@ -1,19 +1,45 @@
+/**
+@license Foswig.js | (c) Glenn Conner. | https://github.com/mrsharpoblunto/foswig.js/blob/master/LICENSE
+*/
 (function(global){
+	
+global['foswig'] = {};
 
-global.foswig = {};
-
-global.foswig.MarkovChain = function(order)
+/** 
+ * @constructor 
+ * @param {number} order The order of the Markov chain. This indicates how many previous characters to take into account when picking the next. A lower number represents more random words, whereas a higher number will result in words that match the input words more closely.
+ */
+global['foswig']['MarkovChain'] = function(order)
 {
-	var Node = function(ch) {
+	/** 
+	 * @private
+	 * @constructor 
+	 * @param {string} ch The character to store in this node
+	 */
+	function Node(ch) {
 		this.character = ch;
 		this.neighbors = [];
 	};
 
-	var startNode = new Node('\0');
+	var startNode = new Node('');
 	var map = {};
 	var words = {};
+	
+	/**
+	 * Adds an array or words to the Markov chain
+	 * @param {Array.<string>} words The words to add to the Markov chain
+	 */
+	this['addWordsToChain'] = function(words) {
+		for (var i=0;i<words.length;++i) {
+			this['addWordToChain'](words[i]);
+		}
+	};
 
-	this.addWordToChain = function(word) {
+	/**
+	 * Adds a single word to the Markov chain
+	 * @param {string} word The word to add to the Markov chain
+	 */
+	this['addWordToChain'] = function(word) {
 		if (words[word]) return;
 		words[word] = true;
 		
@@ -37,29 +63,40 @@ global.foswig.MarkovChain = function(order)
 		//link to end node.
 		previousNode.neighbors.push(null);
 	};
-	
-	this.generateWord = function(maxLength) {
+
+	/**
+	 * Generates a random word based on the Markov chain
+	 * @param {number} maxLength The maximum length of the generated word
+	 * @param {boolean} allowDuplicates Whether generated words are allowed to be the same as words in the input dictionary
+	 */	
+	this['generateWord'] = function(minLength,maxLength,allowDuplicates) {
 		var word;
+		var repeat;
 		do 
 		{
+			repeat = false;
 			var nextNodeIndex = Math.floor(Math.random() * startNode.neighbors.length);
 			var currentNode = startNode.neighbors[nextNodeIndex];
 			word = "";
 			
-			while (currentNode && word.length<maxLength) {
+			while (currentNode && word.length<=maxLength) {
 				word+=currentNode.character;
 				nextNodeIndex = Math.floor(Math.random() * currentNode.neighbors.length);
 				currentNode = currentNode.neighbors[nextNodeIndex];
 			}
+			if ( word.length > maxLength || word.length < minLength ) {
+				repeat = true;
+			}
 		}
-		while (words[word]);
+		// we don't want to output any exact replicas from the input dictionary
+		while (repeat || (!allowDuplicates && words[word]));
 		return word;
 	};
 };
 
-if (typeof global.define === "function" && global.define.amd) {
-  global.define("foswig", [], function() {
-    return global.foswig;
+if (typeof global['define'] === "function" && global['define']['amd']) {
+  global['define']("foswig", [], function() {
+    return global['foswig'];
   });
 }
 
