d0964dd5ecc284850b6d69a291dbfa9263165dd0
Glenn <mr.sharpoblunto@gmail.com>
2013-08-24 17:28:24 -0700

    Now building a duplicates trie on markov chain creation so that generated words that are duplicates of substrings in the input dictionary can be rejected efficiently.

--- foswig.js
+++ foswig.js
@@ -20,10 +20,18 @@ global['foswig']['MarkovChain'] = function(order)
 		this.character = ch;
 		this.neighbors = [];
 	};
-
-	var startNode = new Node('');
+	
+	/** 
+	 * @private
+	 * @constructor
+	 */
+	function TrieNode() {
+		this.children = [];
+	};
+	
+	var duplicates = new TrieNode();	
+	var start = new Node('');
 	var map = {};
-	var words = {};
 	
 	/**
 	 * Adds an array or words to the Markov chain
@@ -35,15 +43,59 @@ global['foswig']['MarkovChain'] = function(order)
 		}
 	};
 
+
+	/**
+	 * @private
+	 * Adds a word and all its substrings to a duplicates trie to
+	 * ensure that generated words are never an exact match or substring
+	 * of a word in the input dictionary. Building a trie allows us
+	 * to efficiently search for these duplicates later without 
+	 * having to do O(N) comparision checks over the entire dictionary
+	 * @param {string} word the word to add to the duplicates trie.
+	 */
+	var addToDuplicatesTrie = function(word) {
+		if (word.length > 1) {
+			addToDuplicatesTrie(word.substr(1));
+		}
+		
+		var currentNode = duplicates;
+		for (var i=0;i<word.length;++i) {
+			var childNode = currentNode.children[word[i]];
+			if (!childNode) {
+				childNode = new TrieNode();
+				currentNode.children[word[i]] = childNode;
+			}
+			currentNode = childNode;
+		}
+	};
+	
+	/**
+	 * @private
+	 * Check to see if a word is a match to any substring in the input
+	 * dictionary in O(N) time, where N is the number of characters in the
+	 * word rather than the number of words in the dictionary.
+	 * @param {string} word The word we want to find out whether it is a 
+	 * duplicate of a substring in the input dictionary.
+	 */
+	var isDuplicate = function(word) {
+		word = word.toLowerCase();
+		var currentNode = duplicates;
+		for (var i=0;i<word.length;++i) {
+			var childNode = currentNode.children[word[i]];
+			if (!childNode) return false;
+			currentNode = childNode;
+		}	
+		return true;
+	};
+	
 	/**
 	 * Adds a single word to the Markov chain
 	 * @param {string} word The word to add to the Markov chain
 	 */
 	this['addWordToChain'] = function(word) {
-		if (words[word]) return;
-		words[word] = true;
-		
-		var previousNode = startNode;
+		addToDuplicatesTrie( word.toLowerCase() );
+				
+		var previous = start;
 		var key = "";
 		for (var i=0;i<word.length; ++i) {
 			var ch = word[i];
@@ -57,11 +109,11 @@ global['foswig']['MarkovChain'] = function(order)
 				map[key] = newNode;
 			}
 			
-			previousNode.neighbors.push(newNode);
-			previousNode = newNode;
+			previous.neighbors.push(newNode);
+			previous = newNode;
 		}
 		//link to end node.
-		previousNode.neighbors.push(null);
+		previous.neighbors.push(null);
 	};
 
 	/**
@@ -75,8 +127,8 @@ global['foswig']['MarkovChain'] = function(order)
 		do 
 		{
 			repeat = false;
-			var nextNodeIndex = Math.floor(Math.random() * startNode.neighbors.length);
-			var currentNode = startNode.neighbors[nextNodeIndex];
+			var nextNodeIndex = Math.floor(Math.random() * start.neighbors.length);
+			var currentNode = start.neighbors[nextNodeIndex];
 			word = "";
 			
 			while (currentNode && word.length<=maxLength) {
@@ -89,7 +141,7 @@ global['foswig']['MarkovChain'] = function(order)
 			}
 		}
 		// we don't want to output any exact replicas from the input dictionary
-		while (repeat || (!allowDuplicates && words[word]));
+		while (repeat || (!allowDuplicates && isDuplicate(word)));
 		return word;
 	};
 };
