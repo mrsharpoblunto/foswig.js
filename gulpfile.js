d5f0b657680fcc0fc9a69b342e629e61d737414c
Glenn <mr.sharpoblunto@gmail.com>
2014-06-28 11:19:12 -0700

    fixed gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,17 @@ var gulp = require('gulp');
 var del = require('del');
 var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
 
 gulp.task('clean',function(cb) {
 	del(['dist'],cb);
 });
 
 gulp.task('build',['clean'],function() {
-	gulp.src('foswig.js')
+	gulp.src('lib/index.js')
 		.pipe(uglify())
 		.pipe(browserify({standalone:'Foswig'}))
+		.pipe(concat('foswig.js'))
 		.pipe(gulp.dest('dist'));
 });
 
